c1379f46d0cab10c7b4bc8f267d6d510560cc4c8
Oscariolo <oscariolopinop21@gmail.com>
2024-09-22 00:33:46 -0500

    ADD-Added base for wind element and script for communicating with player, needs dynamic way to link it

---
+++ player/ElementPowers/wind_element_component.gd
@@ -0,0 +1,22 @@
+extends Node
+@export var player: CharacterBody2D
+@export var max_boost_jump_velocity: float = 800
+@export var slowing_velocity_factor: float = 50
+var current_boost_power: float = 0
+var charging_boost = false
+
+
+func start_charge():
+	player.velocity.y = 0
+	charging_boost = true
+
+func charge_jump():
+	current_boost_power += lerp(current_boost_power,-max_boost_jump_velocity,0.1)
+	if abs(current_boost_power) >= abs(max_boost_jump_velocity):
+		current_boost_power = max_boost_jump_velocity
+		
+	
+func release_jump():
+	player.velocity.y -= current_boost_power
+	current_boost_power = 0
+	charging_boost = false
--- player/player_movement_component.gd
+++ player/player_movement_component.gd
@@ -1,27 +1,34 @@
 extends Node
+
+@export var player: CharacterBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var last_x_input = [0]
 var last_y_input = [0]
 
 #GRAVITY AND HORIZONTAL MOVEMENT
-const MAX_FALL_SPEED = 350.0
-const MAX_WALK_SPEED = 300.0
+@export var MAX_FALL_SPEED = 350.0
+@export var MAX_WALK_SPEED = 300.0
+@export_range(0,1) var on_air_movement_modifier = 0.2
 #const acc = 50.0
 #JUMP MECHANIC
 @export var jump_height : float = 100
 @export var jump_time_to_peak : float = 0.4
-@export var jump_time_to_descent : float = 0.5
-@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
-@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
-@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
-var direction = 0
+@export var jump_time_to_descent : float = 0.3
+var jump_velocity : float
+var jump_gravity : float
+var fall_gravity : float 
 #jump buffer timer
 var jump_timer_buffer = 0.0
+#jump control
+var jumped = false
 
-@onready var player: CharacterBody2D = self.get_parent()
+func _ready() -> void:
+	set_default_motion()
 
 func _process(_delta):	
+	_inputControls()
+
+func _inputControls(): #manage the input from player holding the last input so it can press multiple keys but priorize the last one
 	if Input.is_action_just_pressed("move_left"):
 		if -1.0 not in last_x_input:
 			last_x_input.append(-1.0)
@@ -47,47 +54,65 @@ func _process(_delta):
 	
 func _physics_process(delta):
 	
-	if player.velocity.y < MAX_FALL_SPEED:
+	if player.velocity.y < MAX_FALL_SPEED: #gravity gets updated given if its jumping or not
 		player.velocity.y += get_currentgravity() * delta
-	_walk(delta)
+		
+	_walk_process(delta) #manages walk process with special case for on air apex change
+	_jump_process(delta)
 	
-	if Input.is_action_just_pressed("jump"):
+	
+
+func _walk_process(_delta): 
+	var on_air_modifier = 0
+	if player.is_on_floor(): #acceleration change ensure speed apex
+		on_air_modifier = 0
+	else:
+		on_air_modifier = on_air_movement_modifier
+	if last_x_input.back() == 1:
+		player.velocity.x = lerp(player.velocity.x, MAX_WALK_SPEED, 0.3 + on_air_modifier)
+	if last_x_input.back() == -1:
+		player.velocity.x = lerp(player.velocity.x,-MAX_WALK_SPEED, 0.3 + on_air_modifier)
+	if last_x_input.back() == 0:
+		player.velocity.x = lerp(player.velocity.x,0.0,0.4)	
+
+func _jump_process(delta):
+	if player.is_on_floor():
+		jumped = false
+	
+	if Input.is_action_just_pressed("jump"): #checks first for a jump input right before reaching the floor
 		jump_timer_buffer = 0.1
 	jump_timer_buffer -= delta
 	if jump_timer_buffer > 0:
 		if player.is_on_floor() or $coyote_time.time_left > 0.0:
-			_jump(delta)
-	if Input.is_action_just_released("jump"):
+			player.velocity.y = jump_velocity
+			jump_timer_buffer = 0.0
+			jumped = true
+	
+	if Input.is_action_just_released("jump"): 
 		if player.velocity.y < MAX_FALL_SPEED:
 			player.velocity.y = lerp(player.velocity.y,fall_gravity,0.05)
 	
-	var was_on_floor = player.is_on_floor()
-	player.move_and_slide()
+	var was_on_floor = player.is_on_floor() 
+	player.move_and_slide() #on next frame check if left the floor to start coyote timer
 	var just_left_ledge = was_on_floor and not player.is_on_floor() and player.velocity.y >=0
 	if just_left_ledge:
 		$coyote_time.start()
-
-func _walk(_delta): #TODO refactor code to not suck ass, the logic here is to make sharper turns when jumping
-	var acc = 50
-	if player.is_on_floor(): #acceleration change ensure speed apex
-		acc = acc
-	else:
-		acc = acc * 2
-	if last_x_input.back() == 1:
-		player.velocity.x = min(player.velocity.x + acc,MAX_WALK_SPEED)	
-	if last_x_input.back() == -1:
-		player.velocity.x = max(player.velocity.x - acc,-MAX_WALK_SPEED)
-	if last_x_input.back() == 0:
-		player.velocity.x = lerp(player.velocity.x,0.0,0.4)	
-			
-func _jump(_delta):
-	player.velocity.y = jump_velocity
-	jump_timer_buffer = 0.0
+	
 	
 	
 func get_currentgravity() -> float:
 	return jump_gravity if player.velocity.y < 0.0 else fall_gravity
-	
+
+func set_default_motion() -> void:
+	jump_velocity  = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+	jump_gravity = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+	fall_gravity  = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
+func set_motionless() -> void:
+	jump_velocity = 0
+	jump_gravity = 0
+	fall_gravity = 0
+
 
 func _on_coyote_time_timeout() -> void:
 	pass # Replace with function body.
